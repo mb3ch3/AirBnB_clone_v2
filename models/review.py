3d95e5a3e8ff78714413879a58385d504db89e18
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-09-14 22:19:57 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -9,16 +9,13 @@ from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel, Base):
     """Representation of Review """
-    if models.storage_t == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """initializes Review"""
+        """
+            Init for inherited
+        """
         super().__init__(*args, **kwargs)
